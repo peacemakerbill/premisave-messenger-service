bc24e53468b2c1cd41d4bd968162e2707cb78917
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-13 00:01:05 +0300

    Initial Spring Boot application

--- src/main/java/com/premisave/messenger/PremisaveMessengerServiceApplication.java
+++ src/main/java/com/premisave/messenger/PremisaveMessengerServiceApplication.java
@@ -2,12 +2,26 @@ package com.premisave.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableAsync
 public class PremisaveMessengerServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(PremisaveMessengerServiceApplication.class, args);
+		System.out.println("Premisave Messenger Service started successfully!");
 	}
 
 }
