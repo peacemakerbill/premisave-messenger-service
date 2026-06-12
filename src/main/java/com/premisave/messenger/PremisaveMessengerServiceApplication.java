a9de7f9a5906b3039ed4e0eff232e0323dac6e9f
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-12 23:26:01 +0300

    Initial Spring Boot application

---
+++ src/main/java/com/premisave/messenger/PremisaveMessengerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.premisave.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremisaveMessengerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremisaveMessengerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premisave/messenger/PremisaveMessengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premisave.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremisaveMessengerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
